ef4b49eb4617849a4afef2f2320088420661c9b0
Ekemini <42721358+b4ssey@users.noreply.github.com>
2021-09-25 15:40:32 +0100

    added code verification

---
+++ src/screens/authentication/Verification.js
@@ -0,0 +1,88 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Headline, Paragraph } from "react-native-paper";
+import {
+  CodeField,
+  Cursor,
+  useBlurOnFulfill,
+  useClearByFocusCell,
+} from "react-native-confirmation-code-field";
+
+function Verification(props) {
+  const [value, setValue] = useState("");
+  const ref = useBlurOnFulfill({ value, cellCount: CELL_COUNT });
+  const [props, getCellOnLayoutHandler] = useClearByFocusCell({
+    value,
+    setValue,
+  });
+  const CELL_COUNT = 4;
+  return (
+    <View style={styles.container}>
+      <>
+        <Headline>Get a code?</Headline>
+        <Paragraph>fill your OTP code below</Paragraph>
+      </>
+
+      <>
+        <CodeField
+          ref={ref}
+          {...props}
+          value={value}
+          onChangeText={setValue}
+          cellCount={CELL_COUNT}
+          rootStyle={styles.codeFieldRoot}
+          keyboardType="number-pad"
+          textContentType="oneTimeCode"
+          renderCell={({ index, symbol, isFocused }) => (
+            <View
+              onLayout={getCellOnLayoutHandler(index)}
+              key={index}
+              style={[styles.cellRoot, isFocused && styles.focusCell]}
+            >
+              <Text style={styles.cellText}>
+                {symbol || (isFocused ? <Cursor /> : null)}
+              </Text>
+            </View>
+          )}
+        />
+      </>
+
+      <View style={{ flexDirection: "row" }}>
+        <Paragraph>
+          Didn’t receive code? <Button mode="text">Resend.</Button>.
+        </Paragraph>
+      </View>
+
+      <Button>Let's go</Button>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+  codeFieldRoot: {
+    marginTop: 20,
+    width: 280,
+    marginLeft: "auto",
+    marginRight: "auto",
+  },
+  cellRoot: {
+    width: 60,
+    height: 60,
+    justifyContent: "center",
+    alignItems: "center",
+    borderBottomColor: "#ccc",
+    borderBottomWidth: 1,
+  },
+  cellText: {
+    color: "#000",
+    fontSize: 36,
+    textAlign: "center",
+  },
+  focusCell: {
+    borderBottomColor: "#007AFF",
+    borderBottomWidth: 2,
+  },
+});
+
+export default Verification;
